378132fc77bdf4199930937265bcf364e4e56d04
Rajnish Kumar <RajnishKMehta@proton.me>
2026-08-30 17:26:32 +0530

    Bump version code and name to 14.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,8 +65,8 @@ android {
     applicationId = "moe.rukamori.archivetune"
         minSdk = 26
         targetSdk = 37
-        versionCode = 146
-        versionName = "14.6.0"
+        versionCode = 147
+        versionName = "14.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
