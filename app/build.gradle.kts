de1bdea857edc8a2052ce5d0a0c738696688a59a
Rajnish Kumar <RajnishKMehta@proton.me>
2026-09-13 16:08:15 +0530

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ val hasReleaseSigningConfig =
 android {
     namespace = "moe.rukamori.archivetune"
     compileSdk = 37
-    ndkVersion = "30.0.16248370"
+    ndkVersion = "30.0.16248370" // build
     defaultConfig {
     applicationId = "moe.rukamori.archivetune"
         minSdk = 26
